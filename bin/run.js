f0bfb60a8eb1ec348d5312062146fffa5492af11
Christopher Brenberg <cbrenberg@gmail.com>
2019-02-01 12:04:21 -0600

    configure port variable for deployment

--- bin/run.js
+++ bin/run.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 //require modules
 const service = require('../server/service');
 
+const PORT = process.env.PORT || 3000;
+
 const slackToken = process.env.SLACKBOT_TOKEN;
 const slackClient = require('../server/slackClient');
 
@@ -17,7 +19,7 @@ const rtm = slackClient.init(slackToken, witClient, serviceRegistry);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => {
-  service.listen(process.env.PORT, () => {
-    console.log(`IRIS is listening on port ${process.env.PORT} in ${service.get('env')} mode`);
+  service.listen(PORT, () => {
+    console.log(`IRIS is listening on port ${PORT} in ${service.get('env')} mode`);
   });
 });
