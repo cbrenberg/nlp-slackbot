b2ec583151ec0d0b6be248d36b0777188ef7eb30
Christopher Brenberg <cbrenberg@gmail.com>
2019-02-03 14:37:44 -0600

    log connection data for debugging

--- server/service.js
+++ server/service.js
@@ -10,7 +10,7 @@ service.set('serviceRegistry', serviceRegistry);
 service.put('/service/:intent/:port', (req, res, next) => {
   const serviceIntent = req.params.intent;
   const servicePort = req.params.port;
-  console.log("REMOTE ADDRESS, INTENT AND PORT from service put request:", req.connection.remoteAddress, serviceIntent, servicePort);
+  console.log("req.connection from service put request:", req.connection);
 
   const serviceIp = req.connection.remoteAddress.includes('::')
     ? `[${req.connection.remoteAddress}]`
