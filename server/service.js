ebd10eda4494ce4effd01b9ff16885488936597a
Christopher Brenberg <cbrenberg@gmail.com>
2019-02-03 15:23:59 -0600

    register services by IP once again

--- server/service.js
+++ server/service.js
@@ -13,13 +13,12 @@ service.put('/service/:intent/:port', (req, res, next) => {
   // console.log("req.connection.remoteAddress from service put request:", req.connection.remoteAddress);
   // console.log("req.headers['x-forwarded-for']:", req.headers['x-forwarded-for']);
 
-  // const serviceIp = req.connection.remoteAddress.includes('::')
-  //   ? `[${req.connection.remoteAddress}]`
-  //   : req.connection.remoteAddress;
-
-  // const serviceIp = req.headers['x-forwarded-for'];
-
-  const serviceIp = 'https://dnd-slackbot-service.herokuapp.com';
+  const serviceIp = req.headers['x-forwarded-for']
+    ? req.headers['x-forwarded-for'].split(',')[0]
+    : (req.connection.remoteAddress.includes('::')
+      ? `[${req.connection.remoteAddress}]`
+      : req.connection.remoteAddress
+    );
 
   serviceRegistry.add(serviceIntent, serviceIp, servicePort);
   res.send({ result: `${serviceIntent} at ${serviceIp}:${servicePort}` })
