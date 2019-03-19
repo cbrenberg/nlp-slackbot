96fc1305aed98407cfd1ac323f5deca8f9e711ba
Christopher Brenberg <cbrenberg@gmail.com>
2019-03-18 22:03:29 -0500

    refactor to run as aws lambda

--- server/service.js
+++ server/service.js
@@ -1,10 +1,27 @@
 'use strict'
-
+require('dotenv').config();
 const express = require('express');
 const service = express();
+
+const PORT = process.env.PORT || 3000;
+
+const slackToken = process.env.SLACKBOT_TOKEN;
+const slackClient = require('./slackClient');
+
+const witToken = process.env.WIT_TOKEN
+const witClient = require('./witClient')(witToken);
+
+const rtm = slackClient.init(slackToken, witClient);
+rtm.start();
 
 service.post('/', (req, res, next) => {
   res.send('You have awakened me').status(200);
 })
+
+slackClient.addAuthenticatedHandler(rtm, () => {
+  service.listen(PORT, () => {
+    console.log(`IRIS is listening on port ${PORT} in ${service.get('env')} mode`);
+  });
+});
 
 module.exports = service;
